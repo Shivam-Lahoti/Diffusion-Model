65ab7eb17b62cbea25ba8e17b396422eab6535dd
Shivam-Lahoti <Shivam.2199@gmail.com>
2024-12-09 18:14:14 -0500

    commit

--- Model/backend/app/utils.py
+++ Model/backend/app/utils.py
@@ -9,7 +9,9 @@ def generate_image(prompt, filename="generated_image.png"):
         print(f"Generating image for prompt: {prompt}")
 
         # Ensure the output directory exists
-        output_dir = "E:/Diffusion-Model/env/Model/generated_images"
+        output_dir = "E:/Diffusion-Model/env/Model/generated_image"
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
         
 
         # Generate the image
